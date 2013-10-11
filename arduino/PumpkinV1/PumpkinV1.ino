28be069ada5788621554506b18ba1ce092f9875f
IGetIO <ajfrank@igetno.com>
2013-10-11 11:08:28 -0500

    Adding new arduino code

---
+++ arduino/PumpkinV1/PumpkinV1.ino
@@ -0,0 +1,53 @@
+int myPins[] = {2, 3, 4, 5, 6};
+int inPIR = 10;
+int serialIn;
+int lastPick = 100;
+int currPick;
+boolean  inDialogue = false;
+
+String readString;
+void setup()
+{
+  for (int pinNum = 0; pinNum < 5; pinNum++){
+    pinMode(myPins[pinNum], OUTPUT);
+  }
+  pinMode(inPIR, INPUT);
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  
+  
+  if (digitalRead(inPIR) == HIGH && inDialogue == false){
+     currPick = random(5);
+    if (currPick != lastPick){
+     lastPick =  currPick;
+     Serial.println(currPick);
+     inDialogue = true;
+     }    
+   }
+  
+  if (Serial.available() >0) {
+      char c = Serial.read();  //gets one byte from serial buffer
+      readString += c; //makes the string readString
+    } 
+   if (readString.length() >=5) {
+      //Serial.println(readString);
+     if (readString[0] == 57){
+       inDialogue = false;
+     }
+     
+     for (int i = 0; i < 5; i++){
+    
+    //Serial.println(readString[i]); 
+    
+    if (readString[i] == 49){
+      digitalWrite(myPins[i], HIGH);
+    }else{
+     digitalWrite(myPins[i], LOW);
+    }
+  }
+  readString = "";
+  }
+}
